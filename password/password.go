acdd3200d4ff7e628a425d66d64843b7b91fb059
ljwen17 <ljwen17@grgbanking.com>
2020-01-03 14:35:27 +0800

    ok

--- password/password.go
+++ password/password.go
@@ -6,6 +6,22 @@ import (
 )
 var logger = log.GetLogger("password",log.ERROR)
 
+const (
+	Argon2Hasher       = "argon2"
+	BCryptHasher       = "bcrypt"
+	BCryptSHA256Hasher = "bcrypt_sha256"
+	CryptHasher        = "crypt"
+	MD5Hasher          = "md5"
+	PBKDF2SHA1Hasher   = "pbkdf2_sha1"
+	PBKDF2SHA256Hasher = "pbkdf2_sha256"
+	SHA1Hasher         = "sha1"
+	UnsaltedMD5Hasher  = "unsalted_md5"
+	UnsaltedSHA1Hasher = "unsalted_sha1"
+
+	DefaultHasher = PBKDF2SHA256Hasher
+	DefaultSaltSize = 12
+)
+
 func Encode(password string,saltSize int,hasher string) string {
 	hash, err := unchained.MakePassword(password, unchained.GetRandomString(saltSize), hasher)
 	if err != nil {
